d93f1fb376d7b6fa62022371faf464c515a16551
Lariucci <phellipelariucci@gmail.com>
2025-09-15 09:12:51 -0300

    acrescentando emoji

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Isa, eu te amo, se voce me ama da uma risadinha!</h1>
+    return <h1>Isa, eu te amo, se voce me ama da uma risadinha! &#x1F496;</h1>
 }
 
 export default Home;
